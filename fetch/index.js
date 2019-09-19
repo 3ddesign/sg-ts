2d8128fe7641dd1fc6238fd3dc63e5bcf252d43f
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-09-19 07:37:11 +0300

    fixed test error

--- fetch/index.js
+++ fetch/index.js
@@ -3,5 +3,14 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (resp) {
-    console.log(resp.data);
+    const todo = resp.data;
+    const ID: todo.id;
+    const title: todo.title;
+    const finished: todo.finished;
+
+    console.log(`
+    The todo id: ${ID} 
+    The todo title: ${title} 
+    Finished: ${finished} 
+    `);
 });
--- fetch/index.ts
+++ fetch/index.ts
@@ -3,5 +3,14 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url).then(resp => {
-console.log(resp.data);
+    const todo = resp.data;
+    const ID = todo.id;
+    const title = todo.title;
+    const finished = todo.finished;
+
+    console.log(`
+    The todo id: ${ID} 
+    The todo title: ${title} 
+    Finished: ${finished} 
+    `);
 });
