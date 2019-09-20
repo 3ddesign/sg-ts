cb4f32a349ea0e84952ea991ce0c0909985a5459
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-09-20 07:34:21 +0300

    interface example

--- fetch/index.ts
+++ fetch/index.ts
@@ -2,15 +2,21 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+    id: number,
+    title: string,
+    completed: boolean
+}
+
 axios.get(url).then(resp => {
-    const todo = resp.data;
+    const todo = resp.data as Todo;
     const ID = todo.id;
     const title = todo.title;
-    const finished = todo.finished;
+    const completed = todo.completed;
 
     console.log(`
     The todo id: ${ID} 
     The todo title: ${title} 
-    Finished: ${finished} 
+    Finished: ${completed} 
     `);
 });
