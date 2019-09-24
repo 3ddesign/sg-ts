22f1dc636f2246d4e3f7611d01a4d4ec22305d6c
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-09-24 08:20:52 +0300

    example object literal annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -5,5 +5,23 @@ let hasName: boolean = true;
 let nothingMatch: null = null;
 let nothing: undefined = undefined;
 
-//Buildin objects:
+// Buildin objects:
 let now: Date = new Date();
+
+// Array:
+let colors: string[] = ['red', 'green'];
+let numbers: number[] = [1,2,3];
+let truthes: boolean[] = [false, true];
+
+// Classes: 
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// Object literals
+let point: { x: number; y: number}  = {
+    x: 10,
+    y: 20
+};
