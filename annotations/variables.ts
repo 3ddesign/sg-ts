af22ec12c74ffcbb00eb6a1dbaa0c2fc0163b4d9
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-09-26 07:58:11 +0300

    any annotation

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -30,3 +30,10 @@ let point: { x: number; y: number}  = {
 const logNumer: (i: number) => void = (i: number) => {
     console.log(i);
 };
+
+// When to use annotations
+// 1) Functions returns any type
+const json = '{"x": 10}';
+const coordinates = JSON.parse(json);
+console.log(coordinates);
+
