a07a671233ffd7bc1cb86ecbccfe456be29016b3
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-10-06 08:12:02 +0300

    multiple array types

--- arrays.ts
+++ arrays.ts
@@ -3,3 +3,24 @@ let carMakers: string[];
 carMakers = ['ford', 'toyota', 'chevy'];
 
 const dates: string[][] = [];
+
+// help with inference when extracting variables
+
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// prevent incopatible values
+
+carMakers.pop(100);
+
+// help with map
+
+carMakers.map((car: string): string => {
+    return car.toLocaleLowerCase();
+});
+
+// Flexible types: 
+
+const importantDates: (Date | string)[] = [new Date(), '2014-10-10']
+importantDates.push(new Date());
+importantDates.push('2015-11-11');
