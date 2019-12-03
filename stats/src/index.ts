59b5c96cb137e5c769b9bf7bbee3255d919632f4
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-12-03 07:58:45 +0200

    extract read method

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,13 +1,6 @@
-import fs from 'fs';
+import { CSVFileReader } from './CSVFileReader';
 
-const matches = fs
-    .readFileSync('football.csv', {
-        encoding: 'utf-8'
-    })
-    .split('/n')
-    .map((row: string): string[] => {
-        return row.split(',');
-    });
+const reader = new CSVFileReader('./football.csv');
 
 let manUnitedWins = 0;
 
@@ -17,7 +10,7 @@ enum MatchResult {
     Draw =  'D'
 }
 
-for (let match of matches) {
+for (let match of reader.data) {
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
