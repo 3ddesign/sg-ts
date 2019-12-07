2a389b897ce5198e4a87c1fa2531f6981fdd57f3
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-12-07 08:39:21 +0200

    discribe row with tuple

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -5,6 +5,8 @@ const reader = new CSVFileReader('./football.csv');
 
 let manUnitedWins = 0;
 
+// const dateOfFirstMatch = reader.data[0][0];
+
 for (let match of reader.data) {
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
