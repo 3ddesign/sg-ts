bedf5487083d8c1b5c08d30431a23f4f82c5a748
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2019-12-11 08:08:58 +0200

    interface-based approach

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,5 +1,5 @@
 import { MatchResult } from './MatchResult';
-import { MatchReader } from './MatchReader';
+import { MatchReader } from './inheritance/MatchReader';
 
 const reader = new MatchReader('./football.csv');
 
