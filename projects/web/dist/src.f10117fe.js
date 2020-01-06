9adfaf42a4fa5c827e86c22ece416cda45155ad4
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2020-01-06 07:33:03 +0200

    add json server

--- projects/web/dist/src.f10117fe.js
+++ projects/web/dist/src.f10117fe.js
@@ -129,6 +129,7 @@ var User =
 function () {
   function User(data) {
     this.data = data;
+    this.events = {};
   }
 
   User.prototype.get = function (propName) {
@@ -137,6 +138,24 @@ function () {
 
   User.prototype.set = function (update) {
     Object.assign(this.data, update);
+  };
+
+  User.prototype.on = function (eventName, callback) {
+    var handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  };
+
+  User.prototype.trigger = function (eventName) {
+    var handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(function (callback) {
+      callback();
+    });
   };
 
   return User;
@@ -156,12 +175,13 @@ var user = new User_1.User({
   name: 'myname',
   age: 20
 });
-user.set({
-  name: 'newName',
-  age: 15
+user.on('change', function () {
+  console.log('change1');
 });
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', function () {
+  console.log('change2');
+});
+user.trigger('change');
 },{"./models/User":"src/models/User.ts"}],"../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -190,7 +210,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52474" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49559" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
