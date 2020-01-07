ec3d7a0e81f30a07868be3674f041d42fa47e551
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2020-01-07 08:50:30 +0200

    test axios post

--- projects/web/src/index.ts
+++ projects/web/src/index.ts
@@ -1,13 +1,3 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'myname', age: 20});
-
-user.on('change', () => {
-    console.log('change1');
-
-});
-user.on('change', () => {
-    console.log('change2');
-});
-
-user.trigger('change');
+axios.get('http://localhost:3000/users/1');
