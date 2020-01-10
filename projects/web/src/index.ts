dfb22dd471ed9228018816e57141d29dcbdc0b35
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2020-01-10 07:59:15 +0200

    add save method

--- projects/web/src/index.ts
+++ projects/web/src/index.ts
@@ -1,5 +1,7 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'new name', age: 0 });
 
-user.fetch();
+// user.set({ name: 'new name', age: 20 });
+
+// user.save();
