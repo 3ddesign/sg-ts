450b94c090fbd6ab439d610be4418d2ad588334b
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2020-01-12 14:36:05 +0200

    Composition with Nested Objects

--- projects/web/src/index.ts
+++ projects/web/src/index.ts
@@ -3,5 +3,10 @@ import { User } from './models/User';
 const user = new User({ name: 'new name', age: 0 });
 
 // user.set({ name: 'new name', age: 20 });
-
 // user.save();
+
+user.events.on('change', () => {
+    console.log('change!');
+}) 
+
+user.events.trigger('change');
--- projects/web/src/models/Eventing.ts
+++ projects/web/src/models/Eventing.ts
@@ -1,6 +1,6 @@
 type Callback = () => void;
 
-export class Evenging {
+export class Eventing {
     events: { [key: string]: Callback[] } = {};
 
     on(eventName: string, callback: Callback) {
