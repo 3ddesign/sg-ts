a1e1f511096abc00b7397ef04fdbb3c96d2530b9
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2020-01-23 08:00:59 +0200

    add Setting Data While Triggering

--- projects/web/src/index.ts
+++ projects/web/src/index.ts
@@ -2,10 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new name', age: 0 });
 
-console.log(user.get('name'));
-
 user.on('change', () => {
     console.log('User was changed!');
 });
 
-user.trigger('change');
+user.set({ name: 'test' });
