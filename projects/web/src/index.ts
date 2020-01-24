9caa348862e289309799f9a790941e7233ef69c6
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2020-01-24 07:59:42 +0200

    add Fetching User Data

--- projects/web/src/index.ts
+++ projects/web/src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'new name', age: 0 });
+const user = new User({ id: 1 });
 
 user.on('change', () => {
-    console.log('User was changed!');
+    console.log(user);
 });
 
-user.set({ name: 'test' });
+user.fetch();
