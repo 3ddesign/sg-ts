45d7bc8afdbd53eca4c08dbd13e62a4398c04900
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2020-02-04 07:53:44 +0200

    add cless method for collections

--- projects/web/src/models/User.ts
+++ projects/web/src/models/User.ts
@@ -2,6 +2,7 @@ import { Model } from './Model';
 import { ApiSync } from './ApiSync';
 import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
+import { Collection } from './Collection';
 
 
 export interface UserProps {
@@ -21,7 +22,8 @@ export class User extends Model<UserProps> {
         )
     }
 
-    isAdminUser(): boolean {
-        return this.get('id') === 1;
+    static buildUserCollection(): Collection<User, UserProps> {
+        return new Collection<User, UserProps>(rootUrl,
+        (json: UserProps) => User.buildUser(json));
     }
 }
