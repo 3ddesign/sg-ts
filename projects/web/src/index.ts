7896883341b7da9757267d745eb045cc3e7fdd4c
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2020-02-23 08:23:15 +0200

    add collectionView implementation

--- projects/web/src/index.ts
+++ projects/web/src/index.ts
@@ -1,14 +1,15 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
 
-const user = User.buildUser({ name: 'test', age: 20 });
-const root = document.getElementById('root');
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+    return User.buildUser(json);
+});
 
-if (root) {
-    const userEdit = new UserEdit(root, user);
-    userEdit.render();
-
-    console.log(userEdit);
-} else {
-    throw new Error('Root element not found');
-}
+users.on('change', () => { 
+    const root = document.getElementById('root');
+    
+    if (root) {
+        new UserList(root, users).render();
+    }
+});
