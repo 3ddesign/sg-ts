c0a1067c217d3ee653b017fb59b8d364dca45f7c
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2020-02-25 08:06:33 +0200

    add besic route setting

--- projects/server/src/index.ts
+++ projects/server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('init');
+import express, { Request, Response } from 'express';
+
+const app = express;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`
+    <div>
+    <h1>Test</h1>
+    </div>`);
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
