7a9a5f391f5c01f1ce98993a52c4de2cb18b7ca5
Dmitry Scherbakov <dmitry@scherbakov.com.ua>
2020-03-07 00:01:55 +0200

    add more decorator examples

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,34 +1,57 @@
+@classDecorator
 class Boat {
+    @testDecorator
     color: string = 'red';
 
+    @testDecorator
     get formattedColor(): string {
         return `This boat color is ${this.color}`;
     }
 
-    @logError
-    pilot(): void {
-        throw new Error();
-        console.log('swish');
+    @logError('Oops, boat was sunk')
+    pilot(@parametrDecorator speed: string, @parametrDecorator genareteWake: string): void {
+        // throw new Error();
+        if (speed === 'fast') {
+            console.log('swish');
+        } else {
+            console.log('nothing');
+        }
     }
 }
-//Object.defineProperty(), Object.getOwnPropertyDescriptor(boat, color)
+// Object.getOwnPropertyDescriptor(boat, color)
 // value: "color"
 // writable: true
 // enumerable: true
 // configurable: true
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-    const method = desc.value;
 
-    desc.value = function () {
-        try {
-            method()
-        }
-        catch (e) {
-            console.log('Oops, boat was sunk');
-        }
-    };
+function parametrDecorator(target: any, key: string, index: number) {
+    console.log(key, index);
 }
 
-new Boat().pilot();
+function testDecorator(target: any, key: string) {
+    console.log(key);
+}
+
+function classDecorator(constructor: typeof Boat) {
+    console.log(constructor);
+}
+
+//decorator factory (return function):
+function logError(errorMessage: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor): void {
+        const method = desc.value;
+
+        desc.value = function () {
+            try {
+                method()
+            }
+            catch (e) {
+                console.log(errorMessage);
+            }
+        };
+    }
+}
+
+new Boat().pilot('test', 'test');
 
 // logError(Boat.prototype, 'pilot');
