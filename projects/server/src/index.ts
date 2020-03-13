da1b9590cd93a58f6630ead462c176e7a6280e11
Dmitry <3ddesign@bk.ru>
2020-03-13 07:11:42 +0200

    add using controller

--- projects/server/src/index.ts
+++ projects/server/src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
+import { router as controllerRouter } from './controllers/decorators';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
+
+import './controllers/LoginController';
 
 //TODO: refactor with decorastors:
 
@@ -10,6 +13,7 @@ const app = express;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['dasadasd'] }))
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
