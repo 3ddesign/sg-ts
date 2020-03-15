3a958901462fc1b3f412484c79f7f39173f28fc0
Dmitry <3ddesign@bk.ru>
2020-03-15 07:24:56 +0200

    add routeBinder

--- projects/server/src/controllers/decorators/routes.ts
+++ projects/server/src/controllers/decorators/routes.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
+import { router } from "../../routes/loginRoutes";
 
-function routeFinder(method: string) {
+function routeBinder(method: string) {
   return function(path: string) {
     return function(target: any, key: string, desc: PropertyDescriptor) {
       Reflect.defineMetadata("path", path, target, key);
@@ -8,3 +9,9 @@ function routeFinder(method: string) {
     };
   };
 }
+
+export const get = routeBinder('get');
+export const put = routeBinder('put');
+export const post = routeBinder('post');
+export const del = routeBinder('delete');
+export const patch = routeBinder('patch');
